8727fcbd10a903e720426d420690e0ec860906d0
msmit-CN <158488005+msmit-CN@users.noreply.github.com>
2025-05-02 13:58:07 +0200

    Update main.tf

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -27,7 +27,7 @@ module "vnet" {
     name           = module.naming.virtual_network.name
     location       = module.rg.groups.demo.location
     resource_group = module.rg.groups.demo.name
-    cidr           = ["10.0.0.0/16"]
+    address_space  = ["10.0.0.0/16"]
     subnets = {
       sn1 = {
         cidr = ["10.0.0.0/24"]
